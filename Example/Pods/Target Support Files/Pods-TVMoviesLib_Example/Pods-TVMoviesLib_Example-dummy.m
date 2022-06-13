bc046f43d7b9c9b76f1639d1050937fe2d7e1de5
edo oktarifa <edooktarifa99@gmail.com>
2022-06-13 16:41:53 +0700

    update podspec

---
+++ Example/Pods/Target Support Files/Pods-TVMoviesLib_Example/Pods-TVMoviesLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TVMoviesLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TVMoviesLib_Example
+@end
---
+++ Example/Pods/Target Support Files/TVMoviesLib/TVMoviesLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TVMoviesLib : NSObject
+@end
+@implementation PodsDummy_TVMoviesLib
+@end
